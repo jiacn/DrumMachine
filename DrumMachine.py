98e49d4e366a89ecc58f096adf047a2cc0b2c873
Jia Zhang <jia.zhang.sh@gmail.com>
2017-04-05 11:11:08 +0800

    Creating OOP Based GUI structure

---
+++ DrumMachine.py
@@ -0,0 +1,20 @@
+"""
+Code illustration: 01
+    - Creating OOP Based GUI structure
+"""
+from tkinter import *
+
+PROGRAM_NAME = ' Explosion Drum Machine '
+
+
+class DrumMachine:
+
+    def __init__(self, root):
+        self.root = root
+        self.root.title(PROGRAM_NAME)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    DrumMachine(root)
+    root.mainloop()
